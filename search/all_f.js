e765e33edd2ef0d8f0b0473306ce6e96e786bd94
pantor <pantor@users.noreply.github.com>
2020-11-10 16:05:42 +0000

    deploy: ae6c4fd135f7d6206fa79435301db617fb930b64

--- search/all_f.js
+++ search/all_f.js
@@ -3,6 +3,7 @@ var searchData=
   ['parameter_2ehpp',['parameter.hpp',['../parameter_8hpp.html',1,'']]],
   ['password',['password',['../classfrankx_1_1robot_1_1_robot.html#a803919c9364d7f3bd508ba98d5ee01c7',1,'frankx::robot::Robot']]],
   ['positionhold',['PositionHold',['../structfrankx_1_1_position_hold.html',1,'frankx::PositionHold'],['../structfrankx_1_1_position_hold.html#aeba14c30c84cae21d39f1dab959089c7',1,'frankx::PositionHold::PositionHold()']]],
+  ['power',['Power',['../namespaceotgx.html#a360e32ab51dd1cf67ef194787af5dad1',1,'otgx']]],
   ['prerotate',['prerotate',['../structfrankx_1_1_affine.html#a4b2972de0e9345e76b5f190a57520bba',1,'frankx::Affine']]],
   ['pretranslate',['pretranslate',['../structfrankx_1_1_affine.html#a792cc712b9f7c9603e6907651c9b4ded',1,'frankx::Affine']]],
   ['pybind11_5fmodule',['PYBIND11_MODULE',['../frankx__python_8cpp.html#a166686adeadadeec1b2a596aaac52761',1,'PYBIND11_MODULE(_frankx, m):&#160;frankx_python.cpp'],['../otgx__python_8cpp.html#a9d36b6955ebf12580f02c2738cc17fca',1,'PYBIND11_MODULE(otgx, m):&#160;otgx_python.cpp']]]
