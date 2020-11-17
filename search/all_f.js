65bb94f5290f248dd13c01b1563689ef0478f432
pantor <pantor@users.noreply.github.com>
2020-11-17 13:06:32 +0000

    deploy: 0fd6c3244e54837f21ac88a8982e92c427206cde

--- search/all_f.js
+++ search/all_f.js
@@ -10,7 +10,7 @@ var searchData=
   ['pdq',['pdq',['../classmovex_1_1_segment.html#a3db0902b1b7ba8356f7d473e4af0323c',1,'movex::Segment::pdq()'],['../classmovex_1_1_line_segment.html#a5b46c302f59e27a2b62e98d6efd6fdc6',1,'movex::LineSegment::pdq()']]],
   ['point',['point',['../structmovex_1_1_path_point.html#a16b857666b9228e09569707951dee7c8',1,'movex::PathPoint']]],
   ['positionhold',['PositionHold',['../structfrankx_1_1_position_hold.html',1,'frankx::PositionHold'],['../structfrankx_1_1_position_hold.html#aeba14c30c84cae21d39f1dab959089c7',1,'frankx::PositionHold::PositionHold()']]],
-  ['power',['Power',['../namespacemovex.html#a7d0443880056d581a985f9e0f36811f1',1,'movex']]],
+  ['power',['Power',['../namespacemovex.html#a7d0443880056d581a985f9e0f36811f1',1,'movex::Power(double v, int e)'],['../namespacemovex.html#a67821fe7778e67fa9d603c862f58ad93',1,'movex::Power(double v, double e)']]],
   ['prerotate',['prerotate',['../structmovex_1_1_affine.html#a43cedd63e56122a54ba838ca5cf502d7',1,'movex::Affine']]],
   ['pretranslate',['pretranslate',['../structmovex_1_1_affine.html#a8bfbd907a90204452b5537e572cd4f90',1,'movex::Affine']]],
   ['pybind11_5fmodule',['PYBIND11_MODULE',['../frankx_2python_8cpp.html#a166686adeadadeec1b2a596aaac52761',1,'PYBIND11_MODULE(_frankx, m):&#160;python.cpp'],['../movex_2python_8cpp.html#a6cbba0db8b65cb701d212c81571dd3bf',1,'PYBIND11_MODULE(movex, m):&#160;python.cpp']]],
