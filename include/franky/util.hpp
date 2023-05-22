2db3122efaef178d0ed7a5d63f2c9ca44fd7d178
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-05-22 20:15:42 +0200

    Implemented toStd and toEigen util functions

---
+++ include/franky/util.hpp
@@ -0,0 +1,14 @@
+#include <array>
+#include <Eigen/Core>
+
+template<size_t dims>
+static inline std::array<double, dims> toStd(const Eigen::Matrix<double, dims, 1>& vector) {
+  std::array<double, 7> result;
+  Eigen::Matrix<double, dims, 1>::Map(result.data()) = vector;
+  return result;
+}
+
+template<size_t dims>
+static inline Eigen::Matrix<double, dims, 1> toEigen(const std::array<double, dims>& vector) {
+  return Eigen::Matrix<double, dims, 1>::Map(vector.data());
+}
