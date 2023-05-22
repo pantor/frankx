fa5515d9e77956f1c08f83de09ed72e469c7ebdb
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-05-22 20:16:14 +0200

    Deleted constructors of Waypoint structure

--- include/franky/waypoint.hpp
+++ include/franky/waypoint.hpp
@@ -18,7 +18,7 @@ namespace franky {
     };
 
     RobotPose robot_pose;
-    ReferenceType reference_type;
+    ReferenceType reference_type{ReferenceType::Absolute};
 
     //! Dynamic Waypoint: Relative velocity factor
     double velocity_rel{1.0};
@@ -31,15 +31,6 @@ namespace franky {
 
     //! Path Waypoint: Maximum distance for blending.
     double blend_max_distance{0.0};
-
-
-    explicit Waypoint() : reference_type(ReferenceType::Absolute) {}
-
-    explicit Waypoint(const Affine &affine, ReferenceType reference_type = ReferenceType::Absolute,
-                      std::optional<double> elbow = std::nullopt, double velocity_rel = 1.0,
-                      double blend_max_distance = 0.0, std::optional<double> minimum_time = std::nullopt)
-        : robot_pose(affine, elbow), reference_type(reference_type), velocity_rel(velocity_rel),
-          blend_max_distance(blend_max_distance), minimum_time(minimum_time) {}
 
     RobotPose getTargetRobotPose(const RobotPose &old_robot_pose) const {
       std::optional<double> new_elbow;
