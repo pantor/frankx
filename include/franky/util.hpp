1cf5f27e8c73a1d82e1443b8422a92a04d0ac58e
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-05-24 18:00:23 +0200

    Put util functions in franky namespace

--- include/franky/util.hpp
+++ include/franky/util.hpp
@@ -3,14 +3,16 @@
 #include <array>
 #include <Eigen/Core>
 
-template<size_t dims>
-static inline std::array<double, dims> toStd(const Eigen::Matrix<double, dims, 1>& vector) {
-  std::array<double, 7> result;
-  Eigen::Matrix<double, dims, 1>::Map(result.data()) = vector;
-  return result;
-}
+namespace franky {
+  template<size_t dims>
+  inline std::array<double, dims> toStd(const Eigen::Matrix<double, dims, 1> &vector) {
+    std::array<double, 7> result;
+    Eigen::Matrix<double, dims, 1>::Map(result.data()) = vector;
+    return result;
+  }
 
-template<size_t dims>
-static inline Eigen::Matrix<double, dims, 1> toEigen(const std::array<double, dims>& vector) {
-  return Eigen::Matrix<double, dims, 1>::Map(vector.data());
+  template<size_t dims>
+  inline Eigen::Matrix<double, dims, 1> toEigen(const std::array<double, dims> &vector) {
+    return Eigen::Matrix<double, dims, 1>::Map(vector.data());
+  }
 }
