5ee6046054923ee3d3f85fd16d9a448aec1c8ce7
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-05-24 18:02:56 +0200

    Split waypoint in source and header file

--- include/franky/waypoint.hpp
+++ include/franky/waypoint.hpp
@@ -9,7 +9,6 @@
 
 
 namespace franky {
-
   struct Waypoint {
     enum class ReferenceType {
       Absolute,
@@ -31,22 +30,6 @@ namespace franky {
     //! Path Waypoint: Maximum distance for blending.
     double blend_max_distance{0.0};
 
-    RobotPose getTargetRobotPose(const RobotPose &old_robot_pose) const {
-      std::optional<double> new_elbow;
-      if (robot_pose.elbow_position().has_value() && reference_type == ReferenceType::Relative) {
-        if (!old_robot_pose.elbow_position().has_value())
-          new_elbow = robot_pose.elbow_position();
-        else {
-          new_elbow = robot_pose.elbow_position().value() + old_robot_pose.elbow_position().value();
-        }
-      } else {
-        new_elbow = robot_pose.elbow_position();
-      }
-      RobotPose new_robot_pose = robot_pose.with_elbow_position(new_elbow);
-      if (reference_type == ReferenceType::Absolute)
-        return new_robot_pose;
-      return old_robot_pose.end_effector_pose() * new_robot_pose;
-    };
+    [[nodiscard]] RobotPose getTargetRobotPose(const RobotPose &old_robot_pose) const;;
   };
-
-} // namespace franky
+}  // namespace franky
