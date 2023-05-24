019689d75955b1a60549641c0629cf6250a88764
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-05-24 10:15:36 +0200

    Formatted linear.cpp

--- examples/linear.cpp
+++ examples/linear.cpp
@@ -1,4 +1,5 @@
 #include <franky/franky.hpp>
+
 #include "util.cpp"
 
 
@@ -6,26 +7,26 @@ using namespace franky;
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
-        return -1;
-    }
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
+    return -1;
+  }
 
-    // Connect to the robot
-    Robot robot(argv[1]);
-    robot.automaticErrorRecovery();
+  // Connect to the robot
+  Robot robot(argv[1]);
+  robot.automaticErrorRecovery();
 
-    // Reduce the acceleration and velocity dynamic
-    robot.setDynamicRel(0.15);
+  // Reduce the acceleration and velocity dynamic
+  robot.setDynamicRel(0.15);
 
-    // Define and move forwards
-    auto way = mk_affine(0.0, 0.05, 0.0);
-    auto motion_forward = LinearRelativeMotion(way);
-    robot.move(motion_forward);
+  // Define and move forwards
+  auto way = mk_affine(0.0, 0.05, 0.0);
+  auto motion_forward = std::make_shared<LinearMotion>(RobotPose(way), true);
+  robot.move(motion_forward);
 
-    // And move backwards using the inverse motion
-    auto motion_backward = LinearRelativeMotion(way.inverse());
-    robot.move(motion_backward);
+  // And move backwards using the inverse motion
+  auto motion_backward = std::make_shared<LinearMotion>(RobotPose(way.inverse()), true);
+  robot.move(motion_backward);
 
-    return 0;
+  return 0;
 }
