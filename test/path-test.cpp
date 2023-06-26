c10096d4b20c502b5a55d83d582932589690f235
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-06-26 19:04:35 +0200

    Fixed path-test

--- test/path-test.cpp
+++ test/path-test.cpp
@@ -8,49 +8,50 @@
 #include <franky/path/path.hpp>
 #include <franky/path/time_parametrization.hpp>
 
-
 using namespace franky;
 
+void check_path(const std::vector<PathWaypoint> &waypoints, double blend_max_distance) {
+  CAPTURE(waypoints);
+  CAPTURE(blend_max_distance);
 
-void check_path(const std::vector<franky::Affine>& waypoints, double blend_max_distance) {
-    CAPTURE( waypoints );
-    CAPTURE( blend_max_distance );
+  if (waypoints.size() < 2) {
+    CHECK_THROWS(mk_path_from_waypoints(waypoints, blend_max_distance));
+    return;
+  }
 
-    if (waypoints.size() < 2) {
-        CHECK_THROWS( Path(waypoints, blend_max_distance) );
-        return;
-    }
+  auto path = mk_path_from_waypoints(waypoints, blend_max_distance);
+  auto tp = TimeParametrization(0.001);
 
-    auto path = Path(waypoints, blend_max_distance);
-    auto tp = TimeParametrization(0.001);
+  CHECK(path.length() >= 0.0);
 
-    CHECK( path.get_length() >= 0.0 );
+  auto max_velocity = std::array<double, 7>{{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
+  auto max_acceleration = std::array<double, 7>{{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
+  auto max_jerk = std::array<double, 7>{{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
 
-    auto max_velocity = std::array<double, 7> {{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
-    auto max_acceleration = std::array<double, 7> {{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
-    auto max_jerk = std::array<double, 7> {{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
-
-    auto trajectory = tp.parametrize(path, max_velocity, max_acceleration, max_jerk);
+  auto trajectory = tp.parametrize(path, max_velocity, max_acceleration, max_jerk);
 }
 
-
 TEST_CASE("Path from affines and blending") {
-    srand(44);
-    std::default_random_engine gen;
-    std::uniform_real_distribution<double> dist(0.0, 1.0);
+  srand(44);
+  std::default_random_engine gen;
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
 
-    for (size_t i = 0; i < 1024; i += 1) {
-        size_t n = 2 + 4 * dist(gen);
-        if (i < 2) { // Make a few examples with too few waypoints to test exceptions
-            n = i;
-        }
-
-        std::vector<franky::Affine> waypoints(n);
-        for (size_t j = 0; j < n; j += 1) {
-            waypoints[j] = Affine((Vector7d)Vector7d::Random());
-        }
-        double blend_max = 0.1 * dist(gen);
-
-        check_path(waypoints, blend_max);
+  for (size_t i = 0; i < 1024; i += 1) {
+    size_t n = 2 + 4 * dist(gen);
+    if (i < 2) { // Make a few examples with too few waypoints to test exceptions
+      n = i;
     }
+
+    std::vector<PathWaypoint> waypoints(n);
+    for (size_t j = 0; j < n; j += 1) {
+      auto translation = Vector<3>::Random();
+      auto quaternion = Vector<4>::Random().normalized();
+      auto transformation = Affine().fromPositionOrientationScale(
+          translation, Eigen::Quaterniond(quaternion), Eigen::Vector3d::Ones());
+      waypoints[j] = PathWaypoint{{transformation}};
+    }
+    double blend_max = 0.1 * dist(gen);
+
+    check_path(waypoints, blend_max);
+  }
 }
