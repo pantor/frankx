a8cdaecc85fa5f64b4a8ed18528e46eef3b94eb7
Tim Schneider <tim.schneider1@tu-darmstadt.de>
2023-06-28 17:59:28 +0200

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-with Path("README.md").open() as readme_file:
+with (Path(__file__).resolve().parent / "README.md").open() as readme_file:
     long_description = readme_file.read()
 
 
@@ -30,31 +30,25 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-        # required for auto-detection of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
+        ext_dir = Path(self.get_ext_fullpath(ext.name)).parent.resolve()
 
         build_type = os.environ.get("BUILD_TYPE", "Release")
         build_args = ["--config", build_type]
 
         cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}/".format(ext_dir),
             "-DPYTHON_EXECUTABLE={}".format(sys.executable),
             "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
-            "-DCMAKE_BUILD_TYPE=" + build_type,
-            "-DUSE_PYTHON_EXTENSION=OFF",
+            "-DCMAKE_BUILD_TYPE={}".format(build_type),
             "-DBUILD_EXAMPLES=OFF",
             "-DBUILD_TESTS=OFF",
             "-DBUILD_SHARED_LIBS=OFF",
             "-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE",
-            "-DCMAKE_INSTALL_RPATH={}".format("$ORIGIN"),
+            "-DCMAKE_INSTALL_RPATH=$ORIGIN",
             "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
         ]
 
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
+        Path(self.build_temp).mkdir(exist_ok=True, parents=True)
 
         subprocess.check_call(["cmake", str(Path(".").resolve())] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(["cmake", "--build", ".", "--target", "_franky"] + build_args, cwd=self.build_temp)
