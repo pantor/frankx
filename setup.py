7908aa066cea10fdedb29c44bbc65d2e0353ec09
Tim Schneider <tim.schneider94@t-online.de>
2023-08-23 16:51:37 +0200

    Increased version number to 0.5.0

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="franky-panda",
-    version="0.4.4",
+    version="0.5.0",
     description="High-Level Motion Library for the Franka Panda Robot (fork of frankx)",
     long_description=long_description,
     long_description_content_type="text/markdown",
