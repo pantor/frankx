ff4b22f2e1378b2af1fbf499660a4ac2986b2310
Tim Schneider <tim@robot-learning.de>
2023-09-25 14:18:04 +0200

    Raised version number to 0.6.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="franky-panda",
-    version="0.6.0",
+    version="0.6.1",
     description="High-Level Motion Library for the Franka Panda Robot (fork of frankx)",
     long_description=long_description,
     long_description_content_type="text/markdown",
